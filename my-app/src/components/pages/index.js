e4dfcc28978ce8824bbc39e62a7c11635d21607d
vlad <example@gmail.com>
2023-09-06 20:40:19 +0300

    build

--- my-app/src/components/pages/index.js
+++
@@ -1,9 +0,0 @@
-import HomePage from "./homePage/HomePage";
-import WorkoutExercisePage from "./workoutExercisePage/WorkoutExercisePage";
-import WorkoutPage from "./workoutPage/WorkoutPage";
-import StatisticPage from "./statisticPage/StatisticPage";
-import HistoryPage from './historyPage/HistoryPage';
-import DetailPage from "./detailPage/DetailPage";
-import NotFoundPage from "./404/404Page"
-
-export { HomePage, WorkoutExercisePage, WorkoutPage, StatisticPage, HistoryPage, DetailPage, NotFoundPage}
